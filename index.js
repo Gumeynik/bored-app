88ce1e5230a1a84d28f0cd1ddac293599c39ea69
Aleksandr Kubenko <151834458+Gumeynik@users.noreply.github.com>
2024-04-16 19:12:39 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+
+const section = document.querySelector('.js-section')
+const btnNode = document.querySelector('.js-btn');
+
+
+const changeBackground = () => {
+    section.style.background = 'linear-gradient(to right, red, orange)' ;
+}
+const findEntertainment = () => {
+    fetch('http://www.boredapi.com/api/activity/')
+    .then(data => data.json())
+    .then((res) => { 
+    const activity = res.activity;
+    document.querySelector('.js-text').innerHTML = `${activity}`
+    changeBackground();
+});
+}
+
+btnNode.addEventListener('click', findEntertainment);
